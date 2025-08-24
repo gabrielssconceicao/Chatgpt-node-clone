67ec9b725f989423c08ecc62a0b00e45df213713
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-08-24 17:50:04 -0300

    feat: add database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ChatModule } from './chat/chat.module';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Conversation } from './chat/entities/conversation.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: parseInt(process.env.DATABASE_PORT! || '5432'),
+      username: process.env.DATABASE_USERNAME || 'postgres',
+      password: process.env.DATABASE_PASSWORD || 'postgres',
+      database: process.env.DATABASE_NAME || 'chatgpt_clone',
+      entities: [Conversation],
+      synchronize: true,
+    }),
+    ChatModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
